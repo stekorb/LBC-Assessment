be941855a8c0c53c333ef7cb4aebf0c828026355
Stephan Korb <stephan.korb@anova.com>
2025-09-24 13:08:52 +0100

    Validation fix

--- VacationManager/VacationManager/Common/Validators/Employee/EmployeeCreateDtoValidator.cs
+++ VacationManager/VacationManager/Common/Validators/Employee/EmployeeCreateDtoValidator.cs
@@ -1,10 +1,5 @@
 ﻿using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using VacationManager.Common.Enums;
-using VacationManager.Data;
 using VacationManager.Dto.Employee;
-using VacationManager.Repositories;
-using VacationManager.Repositories.Interfaces;
 
 namespace VacationManager.Common.Validators.Employee
 {
@@ -15,10 +10,6 @@ namespace VacationManager.Common.Validators.Employee
             RuleFor(x => x.Email)
                 .EmailAddress().WithMessage("Invalid email format.")
                 .When(x => !string.IsNullOrEmpty(x.Email));
-
-            RuleFor(x => x.ManagerEmail)
-                .EmailAddress().WithMessage("Invalid email format.")
-                .When(x => !string.IsNullOrEmpty(x.ManagerEmail));
         }
     }
 }
